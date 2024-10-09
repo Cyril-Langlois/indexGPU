e79bd939d5c6d45d65b26d720a895a4e5fe3eb0e
glhote1 <gabriel.l-hote@insa-lyon.fr>
2024-10-09 15:52:59 +0200

    Test6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='indexGPU',
     version='0.1.0',
-    packages=['indexGPU'],
+    packages=find_packages(),
     install_requires=[
         "inichord >= 0.1.13",
 		"cupy-cuda12x >= 13.3.0",
@@ -12,6 +12,11 @@ setup(
 		"pyquaternion >=0.9",
     ],
 	data_files=[('indexGPU', ['indexGPU/Indexation_GUI.ui']),
-    ]
+    ],
+    
+    package_data={
+    'indexGPU': ['indexGPU/*.ui'],  # Inclure tous les fichiers .txt dans le dossier data
+    },
+    
     
 )
