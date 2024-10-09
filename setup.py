ca7553f0e225655632e04e4e8b47d6f0d20cd672
glhote1 <gabriel.l-hote@insa-lyon.fr>
2024-10-09 15:29:59 +0200

    Test5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='indexGPU',
     version='0.1.0',
-    packages=find_packages(),
+    packages=['indexGPU'],
     install_requires=[
         "inichord >= 0.1.13",
 		"cupy-cuda12x >= 13.3.0",
