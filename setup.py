be490164377a7d418c60217eb36eff7e38284695
Langlois Cyril <cyril.langlois@insa-lyon.fr>
2024-12-04 12:19:25 +0100

    changing version number from 0.1.0 to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='indexGPU',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         "inichord >= 0.1.13",
